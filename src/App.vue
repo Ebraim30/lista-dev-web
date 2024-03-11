9262bdd08e4418d18ecb03fb60619d0daa6a7fff
Ebraim30 <ebraim.herrera@gmail.com>
2024-03-11 16:56:54 -0300

    tentando fazer exercicio das cidades

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+const cidadess = ref([
+  'São Paulo',
+  'Rio de Janeiro',
+  'Belo Horizonte',
+  'Salvador',
+  'Fortaleza',
+  'Curitiba',
+  'Manaus',
+  'Recife',
+  'Porto Alegre',
+  'Brasília'
+])
+
+function adicionar(index) {
+  cidadess.value[index].quantidade++
+}
+
+function novo(index) {
+  cidadess.value[index].adicionar
+}
+</script>
+<template>
+  <ul>
+    <li v-for="(cidade, index) in cidadess">
+      {{ cidade.cidades }} <button @click="adicionar(index)"></button>
+      <button @click="novo">adicionar</button>
+    </li>
+  </ul>
+</template>
